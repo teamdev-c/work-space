3b97516163353528faee755140d96f4aa5b70345
seiya_tagami <bepozurapu274@gmail.com>
2023-08-01 23:00:19 +0900

    feat: 説明画面のUIを作成

--- explanation.js
+++ explanation.js
@@ -1,21 +1,12 @@
-{/* <div class="explanation">
-      <div class="explanation_header">
-        <button id="js-back">
-          <i class="fa-solid fa-arrow-left"></i>
-        </button>
-        <h1 class="explanation_header_title">説明</h1>
-      </div>
-      <div class="explanation_text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas tempore repellat molestiae recusandae esse saepe hic unde sequi temporibus distinctio, nesciunt alias incidunt nostrum iusto voluptatibus. Nihil, quis in! Debitis.</div>
-    </div> */}
-
-const explanationButton = document.getElementById("js-explanation");
 const entrance = document.getElementById("js-entrance");
+const explanationButton = document.getElementById("js-explanation");
 const layout = document.getElementById("js-layout");
+const layoutExplanation = document.getElementById("js-layout-explanation");
 
 explanationButton.addEventListener("click", () => {
-  explanation = document.createElement('div');
-  explanation.classList.add('explanation');
-  explanation.innerHTML = `
+  entrance.classList.add("hidden");
+  layoutExplanation.classList.add("explanation");
+  layoutExplanation.innerHTML = `
     <div class="explanation_header">
       <button id="js-back">
         <i class="fa-solid fa-arrow-left"></i>
@@ -24,19 +15,11 @@ explanationButton.addEventListener("click", () => {
     </div>
     <div class="explanation_text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas tempore repellat molestiae recusandae esse saepe hic unde sequi temporibus distinctio, nesciunt alias incidunt nostrum iusto voluptatibus. Nihil, quis in! Debitis.</div>
   `;
-  layout.append(explanation)
-  entrance.classList.add('hidden');
 
   const backButton = document.getElementById("js-back");
   backButton.addEventListener("click", () => {
-    layout.innerHTML = `
-    <div class="entrance">
-      <h1 class="entrance_title">テトリス</h1>
-      <div class="entrance_buttons">
-        <button class="entrance_buttons_button">ゲームスタート</button>
-        <button id="js-explanation" class="entrance_buttons_button">説明</button>
-      </div>
-    </div>
-    `
-  })
-})
+    layoutExplanation.innerHTML = "";
+    layoutExplanation.classList.remove("explanation");
+    entrance.classList.remove("hidden");
+  });
+});
